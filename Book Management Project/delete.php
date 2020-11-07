4c7191362ea85912445e94e9a6148f10881e334c
monicaneill <67064659+monicaneill@users.noreply.github.com>
2020-11-07 14:20:28 +0000

    Update delete.php

--- Book Management Project/delete.php
+++ Book Management Project/delete.php
@@ -1,11 +1,11 @@
 <?php
 include_once 'connect.php';
 
-$del = "DELETE FROM books WHERE ID= $_GET[id];";
+$del = "DELETE FROM books WHERE ID= $_GET[del_id];";
 $result = mysqli_query($db, $del);
 
 	 if (mysqli_query($db, $del)){
-     header("refresh:1; url=retrieve.php");
+     header("Location: retrieve.php");
    }
    else {
      echo "Unable to process your request";
